32d45d3b34c58dfa7e7d940521e78fd0a8ea1544
Anthony Morah <cmmorah1@gmail.com>
2024-05-11 10:16:10 +0100

    update 016

--- design/default/scripts/balance.js
+++ design/default/scripts/balance.js
@@ -1,8 +1,4 @@
-const {saveUserInfo} = require("./mod_global_config");
 var GLOBAL_JUMP = require("mod_global_trans").GLOBAL_JUMP;
-var GET_SHOW_AMOUNT = require("mod_global_funcs").GET_SHOW_AMOUNT;
-
-const BalanceEnquiryUrl = "https://biz.corestepbank.com/wallet/balance-enquiry"
 
 ViewModel("balance", {
     data: {
@@ -11,38 +7,32 @@ ViewModel("balance", {
         loading:false,
         showTip:"Fetching account balance",
         balanceEnquiryRequest:{accountNumber:""},
-        balanceResponse:"0.00",
-        balance:""
+        balance:"0.00"
     },
 
     methods:{
-        _formatInput: function () {
-            if(this.balance === "") this.balanceResponse = "0.00";
-            this.balance = parseFloat(this.balance).toFixed(2);
-            const parts  = this.balance.toString().split(".");
-            parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-            this.balanceResponse = parts.join(".")
-            this.notifyPropsChanged();
-        },
+        // _formatInput: function () {
+        //     if(this.balance === "") this.balanceResponse = "0.00";
+        //     this.balance = parseFloat(this.balance).toFixed(2);
+        //     const parts  = this.balance.toString().split(".");
+        //     parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+        //     this.balanceResponse = parts.join(".")
+        //     this.notifyPropsChanged();
+        // },
 
-        toTransaction(){
-            navigateReplace({
-                target: "transactionPage",
-                data:this.user
-            });
-        },
 
         initHTTPCB: function () {
             Tos.HttpclientCbEvent();
         },
-
 
         readBalance(){
             this.loading = true
             this.balanceEnquiryRequest.accountNumber = this.user.customerOrganisationWallet
             this.notifyPropsChanged();
             function onSuccess(data){
+                console.log('balance response =====> ',JSON.stringify(data))
                 this.loading = false
+                this.balance = data.accountBalance
                 this.notifyPropsChanged();
             }
             function onError(data){
@@ -111,9 +101,7 @@ ViewModel("balance", {
     },
 
     onWillMount: function (req) {
-        if(req){
-            this.user = Tos.GLOBAL_CONFIG.userInfo
-        }
+        this.user = Tos.GLOBAL_CONFIG.userInfo
     },
 
     onMount: function () {
