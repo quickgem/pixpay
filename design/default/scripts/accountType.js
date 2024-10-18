4727f5e2276bf3e299aef6c30daf365329720f91
ndubisi ukonu <quickgemltd@gmail.com>
2024-10-18 22:02:57 +0100

    update account_type features

--- design/default/scripts/accountType.js
+++ design/default/scripts/accountType.js
@@ -3,7 +3,10 @@ var GLOBAL_JUMP = require("mod_global_trans").GLOBAL_JUMP;
 ViewModel("accountType", {
     data: {
         user:"",
-        accountType:"Default"
+        accountType:"Default",
+        isSaving:"#FFFFFF",
+        isCurrent:"#FFFFFF",
+        isDefault:"#FFFFFF"
     },
 
     methods: {
@@ -11,10 +14,39 @@ ViewModel("accountType", {
         selectAccountType:function (arg){
             const that = this;
             that.accountType = arg;
+            switch (that.accountType) {
+                case "Default":
+                    that.isDefault = '#FFC002';
+                    that.isCurrent = '#FFFFFF';
+                    that.isSaving = '#FFFFFF';
+                    break;
+                case "Saving":
+                    that.isDefault = '#FFFFFF';
+                    that.isCurrent = '#FFFFFF';
+                    that.isSaving = '#FFC002';
+                    break;
+                case "Current":
+                    that.isDefault = '#FFFFFF';
+                    that.isCurrent = '#FFC002';
+                    that.isSaving = '#FFFFFF';
+                    break;
+                default:
+                    break;
+            }
+
             that.notifyPropsChanged()
 
             // GLOBAL_JUMP('', that.accountType) //TODO check here for refactoring
 
+        },
+
+        handleCancel:function(){
+            navigateReplace({
+                target: "inputAmt",
+                type: "cancel",
+                close_current:true
+                //data:this.user
+            });
         },
 
         onKeyDown(args) {
@@ -23,9 +55,11 @@ ViewModel("accountType", {
             switch (key) {
                 case "cancel":
                     //TODO PERFORM CANCEL ACTION HERE
+                    this.handleCancel();
                     break;
                 case "return":
                     //TODO PERFORM RETURN ACTION HERE
+                    GLOBAL_JUMP('', this.accountType) //TODO check here for refactoring
                     break;
                 default:
                     break;
