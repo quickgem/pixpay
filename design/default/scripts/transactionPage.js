8cfe303048fa548418083e586d78a5a1449e8b88
Abdulwahab Jatto <ndubisijnr@gmail.com>
2024-10-21 12:43:03 +0100

    added packres file

--- design/default/scripts/transactionPage.js
+++ design/default/scripts/transactionPage.js
@@ -14,7 +14,7 @@ ViewModel("transactionPage", {
         readTransactionRequest:{
             terminalId: "",
             page: 1,
-            pageSize: 100,
+            pageSize: 5,
             searchParam: "",
             startDate: "",
             endDate:"",
